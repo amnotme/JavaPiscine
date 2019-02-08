dbc6b8b465ba6a67f0fb8e2925db7d4a05a4ee86
amnotme <amnotme@icloud.com>
2019-02-08 14:38:03 -0800

    Add Playing Cat to methods module

--- PlayingCat/src/PlayingCat.java
+++ PlayingCat/src/PlayingCat.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class PlayingCat {
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer && (temperature >= 25 && temperature <= 45))
+            return (true);
+        else if (!summer && (temperature >= 25 && temperature <= 35))
+            return (true);
+        else
+            return (false);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+    }
 }
