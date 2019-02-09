87c5aebb4fbd95692979ee0f65add3195425bae0
amnotme <amnotme@icloud.com>
2019-02-08 21:15:04 -0800

    Add Print Number in the Word to control flow module using if else

---
+++ NumberInTheWorld/src/NumberInTheWorld.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+
+public class NumberInTheWorld {
+
+    public static void  printNumberInWord(int number) {
+        if (number == 0)
+            System.out.println("ZERO");
+        else if (number == 1)
+            System.out.println("ONE");
+        else if (number == 2)
+            System.out.println("TWO");
+        else if (number == 3)
+            System.out.println("THREE");
+        else if (number == 4)
+            System.out.println("FOUR");
+        else if (number == 5)
+            System.out.println("FIVE");
+        else if (number == 6)
+            System.out.println("SIX");
+        else if (number == 7)
+            System.out.println("SEVEN");
+        else if (number == 8)
+            System.out.println("EIGHT");
+        else if (number == 9)
+            System.out.println("NINE");
+        else
+            System.out.println("OTHER");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
