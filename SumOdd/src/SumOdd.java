c6c08250edf42fd3b4bd48910eae2b2e76bb2c6b
amnotme <amnotme@icloud.com>
2019-02-09 21:29:17 -0800

    Add Sum Odd revised for control flow module

--- SumOdd/src/SumOdd.java
+++ SumOdd/src/SumOdd.java
@@ -1,2 +1,31 @@
 public class SumOdd {
+
+    public static boolean isOdd(int number) {
+        return (number % 2 == 1);
+    }
+
+    public static int sumOdd(int start, int end) {
+        int sum = 0;
+
+        if (start < 0 || end < 0 || end < start)
+            return (-1);
+
+        if (isOdd(start))
+            ;
+        else
+            start++;
+
+        for (; start <= end; start += 2) {
+            sum += start;
+        }
+        return (sum);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumOdd(1, 100));
+        System.out.println(sumOdd(-1, 100));
+        System.out.println(sumOdd(100, 100));
+        System.out.println(sumOdd(100, -100));
+        System.out.println(sumOdd(100, 1000));
+    }
 }
