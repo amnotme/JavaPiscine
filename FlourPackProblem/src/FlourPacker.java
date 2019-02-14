562cba91e2b960491bb654086a9993e8641ba7bc
amnotme <amnotme@icloud.com>
2019-02-14 13:31:39 -0800

    quick commit

--- FlourPackProblem/src/FlourPacker.java
+++ FlourPackProblem/src/FlourPacker.java
@@ -27,12 +27,16 @@ public class FlourPacker {
             sum += SMALL;
             smallCount--;
         }
-        return (false)
+        return (false);
     }
 
     public static void main(String[] args) {
 
-
+        System.out.println(canPack(1, 0 ,4));
+        System.out.printlnZ(canPack(1, 0, 5));
+        System.out.println(canPack(0, 5, 4));
+        System.out.println(canPack(2, 2, 11));
+        System.out.println(canPack(-3, 2, 12));
     }
 
 }
