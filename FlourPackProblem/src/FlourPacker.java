13cb1f47f9a5c51dfb028dc44ee4780b9ff3d159
amnotme <amnotme@icloud.com>
2019-02-14 14:13:20 -0800

    Add Flour Packer to control flow module

--- FlourPackProblem/src/FlourPacker.java
+++ FlourPackProblem/src/FlourPacker.java
@@ -10,22 +10,24 @@ public class FlourPacker {
         if (bigCount < 0 || smallCount < 0)
             return (false);
 
-        while (bigCount > 0) {
-            if (sum == goal)
-                return (true);
+        while (bigCount > 0 ) {
+
             if (sum + BIG > goal)
                 break;
             sum += BIG;
             bigCount--;
+            if (sum == goal)
+                return (true);
         }
 
         while (smallCount > 0) {
-            if (sum == goal)
-                return (true);
+
             if (sum + SMALL > goal)
                 break;
             sum += SMALL;
             smallCount--;
+            if (sum == goal)
+                return (true);
         }
         return (false);
     }
@@ -33,10 +35,11 @@ public class FlourPacker {
     public static void main(String[] args) {
 
         System.out.println(canPack(1, 0 ,4));
-        System.out.printlnZ(canPack(1, 0, 5));
+        System.out.println(canPack(1, 0, 5));
         System.out.println(canPack(0, 5, 4));
         System.out.println(canPack(2, 2, 11));
         System.out.println(canPack(-3, 2, 12));
+        System.out.println(canPack(0, 5, 6));
     }
 
 }
